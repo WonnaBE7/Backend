13106871cc80ef2c54867a7cf53f67fbb1c5630e
minzzz995 <jmj990905@gmail.com>
2025-07-22 14:20:09 +0900

    Initial commit on dev branch with project structure and .gitignore

--- src/main/java/com/wonnabe/common/config/RootConfig.java
+++ src/main/java/com/wonnabe/common/config/RootConfig.java
@@ -1,5 +1,4 @@
 package com.wonnabe.common.config;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.log4j.Log4j2;
