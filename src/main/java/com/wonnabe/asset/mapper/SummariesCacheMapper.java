069724d646a84d59297d372a5a7f09f10ab6d524
Eunhyeon An <eunhyeon@gmail.com>
2025-07-28 17:51:20 +0900

    [feature] #16 get monthly category summary

--- src/main/java/com/wonnabe/asset/mapper/SummariesCacheMapper.java
+++ src/main/java/com/wonnabe/asset/mapper/SummariesCacheMapper.java
@@ -1,10 +1,19 @@
 package com.wonnabe.asset.mapper;
 
+import com.wonnabe.asset.dto.CategorySummaryDTO;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+import java.util.Map;
+
+@Mapper
 public interface SummariesCacheMapper {
-    // 특정 연월의 총 소비금액 (캐시 테이블 기준)
-    Double getMonthlyTotalConsumption(@Param("userId") String userId, @Param("yearMonth") String yearMonth);
+    // 월별 총 소비금액
+    Double getMonthlyTotalConsumption(@Param("userId") String userId,
+                                      @Param("yearMonth") String yearMonth);
+
+    // 카테고리별 월별 합계
+    List<CategorySummaryDTO> getMonthlyCategorySummary(@Param("userId") String userId,
+                                                       @Param("yearMonth") String yearMonth);
 }
