170b88e5698d731f4c07a0b4b1678982da97fa92
cyLee1111 <flora021111@gmail.com>
2025-07-30 10:18:09 +0900

    [feat] handle IllegalArgumentException

--- src/main/java/com/wonnabe/common/exception/ApiExceptionAdvice.java
+++ src/main/java/com/wonnabe/common/exception/ApiExceptionAdvice.java
@@ -1,5 +1,6 @@
 package com.wonnabe.common.exception;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,20 +8,36 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.core.annotation.Order;
 
 import java.util.NoSuchElementException;
+
 @RestControllerAdvice
 @Order(2)
+@Log4j2
 public class ApiExceptionAdvice {
-    // 404 에러
+
+    // 400 Bad Request - 잘못된 요청 값 처리
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        log.error(e);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .header("Content-Type", "text/plain;charset=UTF-8")
+                .body("잘못된 요청 값입니다");
+    }
+
+    // 404 Not Found - 리소스 없음
     @ExceptionHandler(NoSuchElementException.class)
-    protected ResponseEntity<String> handleIllegalArgumentException(NoSuchElementException e) {
+    protected ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
+        log.error(e);
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .header("Content-Type", "text/plain;charset=UTF-8")
                 .body("해당 ID의 요소가 없습니다.");
     }
-    // 500 에러
+
+    // 500 Internal Server Error - 서버 내부 오류
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<String> handleException(Exception e) {
+        log.error(e);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .header("Content-Type", "text/plain;charset=UTF-8")
