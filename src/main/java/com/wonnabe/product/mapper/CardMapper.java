74dcb5c33d8165586df1e2b3f40edba67dcadf1e
JungwooMoon <mju1591@gmail.com>
2025-07-30 14:23:04 +0900

    [refactor] #9 Update Comment of CardMapper

--- src/main/java/com/wonnabe/product/mapper/CardMapper.java
+++ src/main/java/com/wonnabe/product/mapper/CardMapper.java
@@ -2,19 +2,38 @@ package com.wonnabe.product.mapper;
 
 import com.wonnabe.product.domain.CardProductVO;
 import com.wonnabe.product.domain.UserCardVO;
+import com.wonnabe.product.dto.CardApplyRequestDTO;
 import com.wonnabe.product.dto.UserCardDTO;
 import org.apache.ibatis.annotations.Param;
 
 public interface CardMapper {
-    CardProductVO findById(long productId); // 카드 ID로 카드 정보를 가져옴
+    /**
+     * 카드 ID로 카드 정보를 가져옴
+     * @param productId 카드 Id
+     * @return 카듯 상품 정보
+     */
+    CardProductVO findById(long productId);
 
+    /**
+     * 사용자가 보유 중인 카드의 정보 조회
+     * @param productId 상품 정보
+     * @param userId 사용자 Id
+     * @return 사용자 카드의 상세 정보
+     */
     UserCardVO findUserCardByproductId(
-            @Param("productId") long productId, // Param은 파라미터에 이름을 지정함
+            @Param("productId") long productId,
             @Param("userId") String userId
     ); // 카드 ID와 userID로 사용자 카드 정보를 가져옴
 
+    /**
+     * 사용자가 보유중인 카드의 상세 정보 조회
+     * @param productId 카드 Id
+     * @param userId 사용자 Id
+     * @return 사용자가 보유중인 카드의 상세 정보
+     */
     UserCardDTO findUserCardDetailById(
-            @Param("productId") long productId, // Param은 파라미터에 이름을 지정함
+            @Param("productId") long productId,
             @Param("userId") String userId
     );
+
 }
