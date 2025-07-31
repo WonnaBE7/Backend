8f5d7d9e12f3bfb86597b1afa15e9ca9f97778af
JungwooMoon <mju1591@gmail.com>
2025-07-31 16:27:04 +0900

    [refactor] #25 Update graddle for test

---
+++ src/main/java/com/wonnabe/product/dto/CardApplyRequestDTO.java
@@ -0,0 +1,12 @@
+package com.wonnabe.product.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CardApplyRequestDTO
+{
+    String productType; // 상품의 종류
+    String cardId; // 카드의 아이디
+    String cardType; // 카드의 종류
+    String linkedAccount; // 연결계좌
+}
