45fa27c4cafb8763a298f6ab11f6f619dd0b249a
JungwooMoon <mju1591@gmail.com>
2025-08-01 17:35:19 +0900

    [refactor] #25 Change return format for team format

--- src/main/java/com/wonnabe/product/controller/CardApplyController.java
+++ src/main/java/com/wonnabe/product/controller/CardApplyController.java
@@ -1,6 +1,7 @@
 package com.wonnabe.product.controller;
 
 import com.wonnabe.common.security.account.domain.CustomUser;
+import com.wonnabe.common.util.JsonResponse;
 import com.wonnabe.product.dto.CardApplyRequestDTO;
 import com.wonnabe.product.service.CardService;
 import lombok.extern.log4j.Log4j2;
@@ -27,18 +28,18 @@ public class CardApplyController {
      * @return 반환하지 않음
      */
     @PostMapping("/apply")
-    public ResponseEntity<Void> applyCard(
+    public ResponseEntity<Object> applyCard(
             @RequestBody CardApplyRequestDTO cardApplyRequestDTO,
             @AuthenticationPrincipal CustomUser customuser
     ) {
         if (cardApplyRequestDTO == null) {
-            throw new IllegalArgumentException("cardApplyRequestDTO is null");
+            throw new IllegalArgumentException("잘못된 입력 양식입니다.");
         }
 
         String userId = customuser.getUser().getUserId();
 
         cardService.applyUserCard(cardApplyRequestDTO, userId);
 
-        return ResponseEntity.ok().build();
+        return JsonResponse.ok("카드 신청을 완료되었습니다");
     }
 }
