f0e3fa60a78300f290a667e9cc9fd49348b6bdf5
camellia785 <choiyuna759@gmail.com>
2025-08-01 11:40:22 +0900

    [chore] #30 del import Collectors

--- src/main/java/com/wonnabe/product/service/SavingsRecommendationServiceImpl.java
+++ src/main/java/com/wonnabe/product/service/SavingsRecommendationServiceImpl.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
