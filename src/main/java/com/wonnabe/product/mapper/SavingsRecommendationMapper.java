eec477f2099cc46dd6b11b6aca96f372d6380c34
camellia785 <choiyuna759@gmail.com>
2025-08-01 11:36:34 +0900

    [feat] #30 add SavingsRecommendationMapper & xml

--- src/main/java/com/wonnabe/product/mapper/SavingsRecommendationMapper.java
+++ src/main/java/com/wonnabe/product/mapper/SavingsRecommendationMapper.java
@@ -1,18 +1,17 @@
 package com.wonnabe.product.mapper;
 
 import com.wonnabe.product.domain.SavingsProductVO;
-import com.wonnabe.product.domain.UserInfoVO;
+import com.wonnabe.product.domain.UserIncomeInfoVO;
 import org.apache.ibatis.annotations.Mapper;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Mapper
 public interface SavingsRecommendationMapper {
-    // 모든 예적금 점수 조회
-    List<SavingsProductVO> getAllSavingsScores();
 
     // 사용자의 소득/고용 정보 조회
-    UserInfoVO getUserIncomeInfo(String userId);
+    UserIncomeInfoVO getUserIncomeInfo(String userId);
+
+    // 모든 적금 상품과 점수 조회
+    List<SavingsProductVO> getAllSavingsScores();
 }
