1e539102aa3e8666a17802630ec86aa74d9e9dd1
JungwooMoon <mju1591@gmail.com>
2025-08-01 16:26:48 +0900

    [feat] #25 Implement of CardApply API

--- src/main/java/com/wonnabe/product/service/CardService.java
+++ src/main/java/com/wonnabe/product/service/CardService.java
@@ -1,6 +1,9 @@
 package com.wonnabe.product.service;
 
+import com.wonnabe.product.domain.UserCardVO;
+import com.wonnabe.product.dto.CardApplyRequestDTO;
 import com.wonnabe.product.dto.UserCardDetailDTO;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CardService {
     // 유저 카드의 상세 정보를 반환하는 함수
@@ -12,4 +15,12 @@ public interface CardService {
      * @return
      */
     UserCardDetailDTO findUserCardDetail(long productId, String userId);
+
+    /**
+     * 카드 신청
+     * @param cardApplyRequestDTO 사용자 카드 정보
+     * @param userId 사용자 Id
+     * @throws Exception 사용자 카드를 삽입 후 id를 가져오지 못하면 예외 처리
+     */
+    void applyUserCard(CardApplyRequestDTO cardApplyRequestDTO, String userId);
 }
