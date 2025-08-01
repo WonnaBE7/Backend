604f6a39024fc7b906752bd5021213a7407c5ebb
camellia785 <choiyuna759@gmail.com>
2025-08-01 11:38:08 +0900

    [feat] #30 add UserSavingsController

--- src/main/java/com/wonnabe/product/controller/UserSavingsController.java
+++ src/main/java/com/wonnabe/product/controller/UserSavingsController.java
@@ -5,13 +5,15 @@ import com.wonnabe.product.dto.UserSavingsDetailResponseDto;
 import com.wonnabe.product.service.UserSavingsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/user/savings") // URL 경로를 savings로 명확하게 변경
+@RequestMapping("/api/user/savings")
 @RequiredArgsConstructor
 @Log4j2
 public class UserSavingsController {
@@ -22,7 +24,7 @@ public class UserSavingsController {
     public ResponseEntity<UserSavingsDetailResponseDto> getSavingsDetail(
             @PathVariable("productId") Long productId,
             @AuthenticationPrincipal CustomUser customUser
-            ) {
+    ) {
 
         String userId = customUser.getUser().getUserId();
         UserSavingsDetailResponseDto savingsDetail = userSavingsService.getSavingsDetail(userId, productId);
