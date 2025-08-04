e3ee74785e8e6bf7d0f7adb52ff8e674e22a7022
camellia785 <choiyuna759@gmail.com>
2025-08-04 09:30:12 +0900

    [fix] #27 Add SavingsRecommendationServiceImpl Qualifier

--- src/main/java/com/wonnabe/product/controller/RecommendSavingsController.java
+++ src/main/java/com/wonnabe/product/controller/RecommendSavingsController.java
@@ -2,9 +2,11 @@ package com.wonnabe.product.controller;
 
 import com.wonnabe.common.security.account.domain.CustomUser;
 import com.wonnabe.product.dto.SavingsRecommendationResponseDTO;
+import com.wonnabe.product.service.CardService;
 import com.wonnabe.product.service.SavingsRecommendationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/recommendations/savings")
-@RequiredArgsConstructor
 @Log4j2
-public class RecommendSavingsController {
+public class RecommendSavingsController{
+        private final SavingsRecommendationService savingsRecommendationService;
 
-    private final SavingsRecommendationService savingsRecommendationService;
+        public RecommendSavingsController(@Qualifier("SavingsRecommendationServiceImpl") SavingsRecommendationService savingsRecommendationService) {
+        this.savingsRecommendationService =savingsRecommendationService;
+}
+
 
     @GetMapping
     public ResponseEntity<SavingsRecommendationResponseDTO> recommendSavings(
