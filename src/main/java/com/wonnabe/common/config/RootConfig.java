3bcabd57e18195ec5dec33eb126af022f2c0e2e6
camellia785 <choiyuna759@gmail.com>
2025-08-04 11:35:47 +0900

    [chore] #27 Add annotation

--- src/main/java/com/wonnabe/common/config/RootConfig.java
+++ src/main/java/com/wonnabe/common/config/RootConfig.java
@@ -9,10 +9,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
