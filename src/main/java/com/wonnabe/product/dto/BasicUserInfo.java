26a9395af355baa1e0959b6c3a8589b92ab53d22
JungwooMoon <126760153+JungwooMoon@users.noreply.github.com>
2025-08-05 14:05:38 +0900

    [feat] #35 Add DTO for CardProductDetail

---
+++ src/main/java/com/wonnabe/product/dto/BasicUserInfo.java
@@ -0,0 +1,32 @@
+package com.wonnabe.product.dto;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BasicUserInfo {
+	private String userId;
+	private String favoriteProductsByType;
+	private long incomeAnnualAmount;
+	private int nowMeId;
+	private double previousConsumption;
+
+	// 내 카드 Id 리스트로 변환
+	public List<Long> getMyFavorite() {
+		// 예: ["1", "2"] → List<Long>로 변환
+		String ids = favoriteProductsByType.replaceAll("[\\[\\]\\s\"]", ""); // 대괄호, 공백, 쌍따옴표 제거
+		return Arrays.stream(ids.split(","))
+			.filter(s -> !s.isEmpty())
+			.map(Long::parseLong)
+			.collect(Collectors.toList());
+	}
+}
