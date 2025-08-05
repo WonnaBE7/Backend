dda279cca299c2ebd86060051bf12025ce537616
camellia785 <choiyuna759@gmail.com>
2025-08-05 15:32:34 +0900

    [feat] #47 Add SavingsApplyRequestDTO

---
+++ src/main/java/com/wonnabe/product/dto/SavingsApplyRequestDTO.java
@@ -0,0 +1,17 @@
+package com.wonnabe.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SavingsApplyRequestDTO {
+    private Long productId;         // 상품 ID
+    private Long principalAmount;   // 가입 원금 (초기 납입금)
+    private Long monthlyPayment;    // 월 납입액 (적금)
+    private Integer joinPeriod;     // 가입 기간 (개월)
+}
