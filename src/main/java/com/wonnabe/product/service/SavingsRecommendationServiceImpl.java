ea15cbf5efc05fd2d91af2d97b83847fd09fdeb6
camellia785 <choiyuna759@gmail.com>
2025-08-07 11:21:13 +0900

    [chore] #41 change private to public

--- src/main/java/com/wonnabe/product/service/SavingsRecommendationServiceImpl.java
+++ src/main/java/com/wonnabe/product/service/SavingsRecommendationServiceImpl.java
@@ -125,6 +125,11 @@ public class SavingsRecommendationServiceImpl implements SavingsRecommendationSe
         return response;
     }
 
+    @Override
+    public Map<Integer, double[]> getPersonaWeights() {
+        return PERSONA_WEIGHTS;
+    }
+
     // 소득/고용상태에 따른 가중치 조정
     private double[] adjustWeightsByIncome(double[] weights, String incomeSource, String employment) {
         double[] adjusted = weights.clone();
@@ -168,7 +173,7 @@ public class SavingsRecommendationServiceImpl implements SavingsRecommendationSe
     }
 
     // 점수 계산
-    private double calculateScore(SavingsProductVO score, double[] weights) {
+    public double calculateScore(SavingsProductVO score, double[] weights) {
         return (weights[0] * score.getInterestRateScore() +
                 weights[1] * score.getCompoundInterestScore() +
                 weights[2] * score.getPreferentialScore() +
