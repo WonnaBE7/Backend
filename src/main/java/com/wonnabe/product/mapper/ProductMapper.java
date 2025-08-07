f258cd304ced71b124a20b2e6fd481654efccd3b
camellia785 <choiyuna759@gmail.com>
2025-08-07 11:24:40 +0900

    [feat] #41 add ProductMappper for Detail

---
+++ src/main/java/com/wonnabe/product/mapper/ProductMapper.java
@@ -0,0 +1,12 @@
+package com.wonnabe.product.mapper;
+
+import com.wonnabe.product.domain.SavingsProductVO;
+import com.wonnabe.product.dto.BasicUserInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ProductMapper {
+    SavingsProductVO findSavingProductById(String productId);
+
+    BasicUserInfo findBasicUserInfoById(String userId);
+}
