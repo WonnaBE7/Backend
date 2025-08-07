e98cd80701e4877acb4acf87109dc17c98023f46
camellia785 <choiyuna759@gmail.com>
2025-08-07 17:17:10 +0900

    [fix] #41 rename VO

--- src/main/java/com/wonnabe/product/domain/InsuranceProductVO.java
+++ src/main/java/com/wonnabe/product/domain/InsuranceProductVO.java
@@ -24,11 +24,11 @@ public class InsuranceProductVO {
     private BigDecimal malePremium;
 
     // Recommend를 위한 Score + 상세보기 Chart data
-    private Float priceCompetitivenessScore; // 가격 경쟁력 점수
-    private Float coverageLimitScore; // 보장 한도 점수
-    private Float coverageScopeScore; // 보장 범위 점수
-    private Float deductibleScore; // 자기 부담금 점수
-    private Float refundScopeScore; // 환급 범위 점수
+    private Float scorePriceCompetitiveness; // 가격 경쟁력 점수
+    private Float scoreCoverageLimit; // 보장 한도 점수
+    private Float scoreCoverageScope; // 보장 범위 점수
+    private Float scoreDeductibleLevel; // 자기 부담금 점수
+    private Float scoreRefundScope; // 환급 범위 점수
 
     // --- 보험 상세보기를 위한 변수 ---
     private String coverageType;
