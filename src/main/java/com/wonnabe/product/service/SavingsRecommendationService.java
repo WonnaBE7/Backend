9b2f3de51b458e50726ee01842f613a780374460
camellia785 <choiyuna759@gmail.com>
2025-08-18 01:10:58 +0900

    [fix] #125 change calculateFinalAchievementRateService Logic

--- src/main/java/com/wonnabe/product/service/SavingsRecommendationService.java
+++ src/main/java/com/wonnabe/product/service/SavingsRecommendationService.java
@@ -7,8 +7,9 @@ import java.util.Map;
 
 public interface SavingsRecommendationService {
     SavingsRecommendationResponseDTO recommendSavings(String userId, int topN);
-
-    double calculateScore(SavingsProductVO score, double[] weights);
-
     Map<Integer, double[]> getPersonaWeights();
+    double calculateScore(SavingsProductVO score, double[] weights);
+    double[] adjustWeightsByIncome(double[] weights, String incomeSource, String employment);
+    double[] normalizeWeights(double[] weights);
 }
+
